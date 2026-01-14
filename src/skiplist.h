d63f089ccc243b02fb8b3791f560eb30df9c61ca
Aaryan Singh <aaryan.jsingh@gmail.com>
2026-01-13 17:16:06 -0700

    feat: new function definitions

--- src/skiplist.h
+++ src/skiplist.h
@@ -70,20 +70,38 @@ struct SkipList<Key, Comparator>::Node {
 
     Node* Next(int n){
         assert(n >= 0);
-        return next_[n].load(std::memory_order_acquire);
+        // we used load with an order memory_order_acquire to avoid race conditions and load all changes before store was called.
+        return next_[n].load(std::memory_order_acquire);    
     }
 
-    void SetNext(int n){
+    void SetNext(int n, Node* x){
         assert(n>=0);
-        next_[n].store(std::memory_order_release);
+        next_[n].store(x, std::memory_order_release);
     }
-    
+
 private:
     std::atomic<Node*> next_[1];
 };
 
+template <typename Key, class Comparator>
+typename SkipList<Key, Comparator>::Node* SkipList<Key, Comparator>::NewNode(const Key& key, int height) {
+    size_t size = sizeof(Node) + sizeof(std::atomic<Node*>) * (height - 1);
+    void* mem = malloc(size);
+    return new (mem) Node(key);
+}
+
+
+template <typename Key, class Comparator>
+SkipList<Key, Comparator>::SkipList(Comparator cmp)
+    : head_(NewNode(Key(), kMaxHeight)),
+      compare_(cmp),
+      max_height_(1) {
+    for (int i = 0; i < kMaxHeight; i++) {
+        head_->SetNext(i, nullptr);
+    }
 
 
 }
 
+}
 #endif
