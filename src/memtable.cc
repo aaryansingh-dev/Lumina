16114daa084276f0a55a27a3cc73ee414d4b7978
Aaryan Singh <aaryan.jsingh@gmail.com>
2026-01-28 00:11:15 -0700

    feat: added Get function to the Memtable

--- src/memtable.cc
+++ src/memtable.cc
@@ -1,10 +1,14 @@
 #include "memtable.h"
-#include "common.h"
+#include "dbformat.h"
 #include "coding.h"
 #include <algorithm>
+#include <cstring>
 
 
 namespace lumina{
+
+    constexpr int keyLengthSize = 5;
+    constexpr int tagSize = 8;
 
 /**
  * @brief Helper to extract the InternalKey slice from a raw MemTable entry.
@@ -24,7 +28,7 @@ int MemTable::KeyComparator::operator()(const char* a, const char* b) const {
     Slice internalKey_a = GetInternalKeyFromEntry(a);
     Slice internalKey_b = GetInternalKeyFromEntry(b);
 
-    return comparator(a, b);
+    return comparator(internalKey_a, internalKey_b);
 }
 
 /**
@@ -32,7 +36,7 @@ int MemTable::KeyComparator::operator()(const char* a, const char* b) const {
  */
 static Slice GetLengthPrefixedSlice(const char* data) {
     uint32_t len;
-    const char* p = GetVarint32Ptr(data, data + 5, &len);
+    const char* p = GetVarint32Ptr(data, data + keyLengthSize, &len);
     return Slice(p, len);
 }
 
@@ -53,7 +57,7 @@ void MemTable::Add(uint64_t sequence, ValueType type, const Slice& key, const Sl
     // first get all the lengths of key and value
     size_t key_size = key.size();
     size_t value_size = value.size();
-    size_t internal_key_size = key_size + 8;   // this 8 bytes stores 7 byte sequence + 1 byte type
+    size_t internal_key_size = key_size + tagSize;   // this 8 bytes stores 7 byte sequence + 1 byte type
     
     size_t encoded_length = VarintLength(key_size) + key_size + VarintLength(value_size) + value_size;
 
@@ -63,7 +67,7 @@ void MemTable::Add(uint64_t sequence, ValueType type, const Slice& key, const Sl
     char* p = EncodeVarint32(buf, static_cast<uint32_t>(internal_key_size));
     std::memcpy(p, key.data(), key_size);
     p+=key_size;
-    EncodeFixed64(p, (sequence << 8)|type);
+    EncodeFixed64(p, (sequence << tagSize)|type);
     p+=8;
     p = EncodeVarint32(p, static_cast<uint32_t>(value_size));
     std::memcpy(p, value.data(), value_size);
@@ -77,6 +81,38 @@ void MemTable::Add(uint64_t sequence, ValueType type, const Slice& key, const Sl
 
 bool MemTable::Get(const LookupKey& key, std::string* value, Status* s) {
     
+    Slice memkey = key.memtable_key();  // [keyLen][key | sequence | type]
+    Table::Iterator iter(&table_);
+    iter.Seek(memkey.data());  // move the iterator to this value - the first node with user_key
+
+
+    if (iter.Valid()){
+        const char* entry = iter.key();
+        uint32_t key_length;
+        const char* key_ptr = GetVarint32Ptr(entry, entry + keyLengthSize, &key_length);
+
+        // extract user key without tag
+        Slice user_key_in_memtable(key_ptr, key_length - tagSize);
+        if (user_key_in_memtable.compare(key.user_key()) == 0){
+            // user key is same: excluding tag and prefix length
+            uint64_t tag = DecodeFixed64(key_ptr + key_length - tagSize);
+            switch (static_cast<ValueType>(tag & 0xff)) {
+                case kTypeValue: {
+                    // Extract value bytes located after the InternalKey
+                    Slice v = GetLengthPrefixedSlice(key_ptr + key_length);
+                    value->assign(v.data(), v.size());
+                    return true;
+                }
+                case kTypeDeletion:
+                    *s = Status::NotFound("");
+                    return true;
+            } 
+        }
+
+    }
+
+    return false;
+
 }
 
 
